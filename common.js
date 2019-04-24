ff2dd71cc6d59fc29c37c7e820ad1bd3859c1448
Tony Mobily <tonymobily@gmail.com>
2019-04-24 21:23:51 +0800

    Fixec first ever but

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 
 // Methods/properties that makes sense to reflect
-export const defaultReflectedProperties = ['blur', 'click', 'focus', 'value']
+export const defaultReflectedProperties = ['blur', 'click', 'focus', 'name', 'value']
 
 // Attributes that makes sense to reflect
 export const defaultReflectedAttributes = ['autocomplete', 'autofocus', 'disabled', 'name', 'readonly', 'required', 'value']
