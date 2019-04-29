fd1f4849e5fb9c0a59833df14fad46164e63162e
Tony Mobily <tonymobily@gmail.com>
2019-04-29 22:55:16 +0800

    More form automation, finally loading a record

--- Form.js
+++ Form.js
@@ -4,23 +4,57 @@ import { defaultReflectedProperties } from './common.js'
 
 class Form extends CommonMixin(LitElement) {
 
-  static get properties() {
+  static get properties () {
     return {
-      action: {
+      recordId: {
         type: String
       }
     }
   }
 
-  get reflectedProperties() {
+  get reflectedProperties () {
     return ['submit', 'reset', 'checkValidity', 'reportValidity', 'requestAutocomplete', 'elements', 'length', 'name', 'method', 'target', 'action', 'encoding', 'enctype', 'acceptCharset', 'autocomplete', 'noValidate']
   }
 
-  get reflectedAttributes() {
-    return ['blur', 'click', 'focus', 'name', 'accept-charset', 'action', 'autocapitalize', 'autocomplete', 'enctype', 'method', 'novalidate', 'target', ]
+  get reflectedAttributes () {
+    return ['blur', 'click', 'focus', 'name', 'accept-charset', 'action', 'autocapitalize', 'autocomplete', 'enctype', 'method', 'novalidate', 'target' ]
   }
 
-  render() {
+  getFormElements () {
+     return [ ...this.querySelectorAll('[name]') ]
+  }
+
+  setFormElementValues (v) {
+    for (let prop in v) {
+      var el = this.querySelector(`[name=${prop}]`) || this.querySelector(`[nn-name=${prop}]`)
+      if (el) el.value = v[prop]
+    }
+  }
+
+  async updated (changedProperties) {
+    // Load the data
+
+    // If no-autoload is set to true, or there is no autoload or no recordId,
+    // simply give up: nothing to do
+    if (this.getAttribute('no-autoload') || !changedProperties.has('recordId')) return
+
+    // Work out the action's URL, adding the record ID  at the end
+    // (It will be a get)
+    // If there is a result, fetch the element values
+    var action = this.getAttribute('action')
+    var recordId = changedProperties.get('recordId')
+    var fetched
+    if (action) {
+      try {
+        fetched = await fetch(action + '/' + this.recordId)
+      } catch (e) {
+      }
+      var v = await fetched.json()
+      this.setFormElementValues(v)
+    }
+  }
+
+  render () {
     return html`<form id="_el">
                   <slot></slot>
                 </form>`
