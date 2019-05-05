5ddbb0f2649ddc8ccadcc949fb8fd42fd5353429
Tony Mobily <tonymobily@gmail.com>
2019-05-05 21:15:17 +0800

    Reflecting disabled property

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 
 // Methods/properties that makes sense to reflect
-export const defaultReflectedProperties = ['blur', 'click', 'focus', 'name', 'value']
+export const defaultReflectedProperties = ['blur', 'click', 'focus', 'name', 'value', 'disabled']
 
 // Attributes that makes sense to reflect
 export const defaultReflectedAttributes = ['autocomplete', 'autofocus', 'disabled', 'name', 'readonly', 'required', 'value']
