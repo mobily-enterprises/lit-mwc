7ad896b191ad27007f0ce74aa6c73c3bb2a5bd32
Tony Mobily <tonymobily@gmail.com>
2019-05-12 14:48:23 +0800

    Better docs to common

--- common.js
+++ common.js
@@ -1,4 +1,14 @@
+// ALL VISIBLE FORM ELEMENTS https://developer.mozilla.org/en-US/docs/Web/HTML/Element#Forms
+// - button        https://developer.mozilla.org/en-US/docs/Web/API/HTMLButtonElement
+// - input (HUGE)  https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
+// - meter N       https://developer.mozilla.org/en-US/docs/Web/API/HTMLMeterElement
+// - progress N    https://developer.mozilla.org/en-US/docs/Web/API/HTMLProgressElement
+// - select        https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement
+// - textarea      https://developer.mozilla.org/en-US/docs/Web/API/HTMLTextAreaElement
+// * They all implement 'setCustomValidity', 'checkValidity', 'reportValidity' except meter and progress
+//
 
+//
 // HTML Methods (no properties) from HTMLElements https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement#Methods
 export const HTMLBasePropsAndMethods = ['blur', 'click', 'focus']
 
