56dc2a6891bbadc4abc06c09e46a3e132c5eda55
Tony Mobily <tonymobily@gmail.com>
2019-05-12 12:26:36 +0800

    Linted

--- test-element.js
+++ test-element.js
@@ -1,28 +1,28 @@
-import { LitElement, html, css } from 'lit-element';
-import './InputText.js';
-import { DefaultTheme } from './BaseStyle.js';
+import { LitElement, html } from 'lit-element'
+import './InputText.js'
+import { DefaultTheme } from './BaseStyle.js'
 
 class TestElement extends LitElement {
-   static get properties() {
-     return {
-       path: { type: String }
-     };
-   }
+  static get properties () {
+    return {
+      path: { type: String }
+    }
+  }
   static get styles () {
     return DefaultTheme
   }
 
-  constructor() {
+  constructor () {
     super()
-    this.path="./input-style.css"
+    this.path = './input-style.css'
   }
-  
-  render() {
+
+  render () {
     return html`
       <nn-input-text labelBefore .stylesheet="${this.path}">
         <label slot="label">Test label</label>
       </nn-input-text>
-    `;
+    `
   }
 }
-customElements.define('test-element', TestElement);
+customElements.define('test-element', TestElement)
