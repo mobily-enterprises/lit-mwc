1b68d4de21730d0a3d4516ea63256863af620d60
Tony Mobily <tonymobily@gmail.com>
2019-07-23 07:28:35 +0800

    Linted

--- Button.js
+++ Button.js
@@ -14,15 +14,15 @@ class Button extends CommonMixin(LitElement) {
       }
 
       button[raised] {
-        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 
-                    0 1px 5px 0 rgba(0, 0, 0, 0.12), 
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+                    0 1px 5px 0 rgba(0, 0, 0, 0.12),
                     0 3px 1px -2px rgba(0, 0, 0, 0.2);
       }
     `
   }
   static get properties () {
     return {
-      stylesheet: { type: String },
+      stylesheet: { type: String }
     }
   }
 
