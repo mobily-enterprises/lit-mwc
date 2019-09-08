959fcd3cf3510031601f89065991176dcd06dc7f
Raphael Mattos <raphael.mattos@contextaware.com.br>
2019-09-08 20:07:15 -0300

    input text dependency for password

--- InputPassword.js
+++ InputPassword.js
@@ -1,4 +1,5 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css } from 'lit-element'
+import './InputText'
 
 export class InputPassword extends InputText {
   render () {
