d24253bc99342a28c48737c151874d9bf41e2935
Raphael Mattos <raphael.mattos@contextaware.com.br>
2019-09-08 19:54:09 -0300

    new inputPassword

---
+++ InputPassword.js
@@ -0,0 +1,16 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class InputPassword extends InputText {
+  render () {
+    return html`
+                ${this.customStyle}
+
+                ${this.labelBeforeTemplate}
+
+                <input type="password" id="_native">
+
+                ${this.labelAfterTemplate}
+              `
+  }
+}
+window.customElements.define('nn-input-password', InputPassword)
