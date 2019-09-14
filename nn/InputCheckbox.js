728ef084cd11f50acc57831fa5ec8e332a5582b8
Tony Mobily <tonymobily@gmail.com>
2019-09-15 07:59:32 +0800

    Styled labels for checkboxes

--- nn/InputCheckbox.js
+++ nn/InputCheckbox.js
@@ -8,6 +8,28 @@ import { baseProperties, defaultBootProperties, inputIDLProperties, alwaysSkipAt
 class InputCheckbox extends FormElementMixin(LabelsMixin(StyleableMixin(NativeReflectorMixin(LitElement)))) {
   static get styles () {
     return css`
+      label {
+        display: inline-flex;
+        font-size: 1em;
+        border: var(--nn-label-border, 1px solid #dddddd);
+        color: var(--nn-label-color, inherit);
+        background-color: var(--nn-label-background, #eeeeee);
+        border-radius: var(--nn-label-border-radius, 4px 0 0 4px );
+        padding-left: 4px;
+        padding-right: 4px;
+        min-width: fit-content;
+        margin-right: -5px;
+        white-space: nowrap;
+      }
+
+      label div#label-text, , ::slotted(*) {
+        align-self: center;
+        width: var(--nn-input-label-width, auto);
+      }
+
+      input:invalid + label {
+        background-color: var(--nn-label-background-invalid, #dd9999);
+      }
     `
   }
 
