fcd63655f7a18554efc62de9572346cb02815628
Tony Mobily <tonymobily@gmail.com>
2019-09-16 10:24:52 +0800

    nn-input-range simplified to be nearly native

--- nn/InputRange.js
+++ nn/InputRange.js
@@ -2,32 +2,12 @@ import { html } from 'lit-element'
 import { InputText } from './InputText'
 
 class InputRange extends InputText {
-  static get properties () {
-    return {
-      shownValue: {
-        type: String,
-        attribute: false
-      }
-    }
-  }
-
-  firstUpdated () {
-    super.firstUpdated()
-    this.shownValue = this.shadowRoot.querySelector('#native').value
-  }
-
   render () {
     return html`
       ${this.customStyle}
       ${this.ifLabelBefore}
-      <input @change=${this.updateShownValue} type="range" id="native">
-      <span>VALUE: -${this.shownValue}-</span>
-      ${this.ifLabelAfter}
-              `
-  }
-
-  updateShownValue (e) {
-    this.shownValue = e.srcElement.value
+      <input type="range" id="native">
+      ${this.ifLabelAfter}              `
   }
 }
 window.customElements.define('nn-input-range', InputRange)
