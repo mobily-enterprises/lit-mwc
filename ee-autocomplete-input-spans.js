1e951db540f2e0555d9d56efe1f1c180ed00b3be
Raphael Mattos <raphael.mattos@contextaware.com.br>
2019-09-30 19:20:27 -0300

    autocomplete-input-spans style wip

--- ee-autocomplete-input-spans.js
+++ ee-autocomplete-input-spans.js
@@ -37,12 +37,32 @@ class EeAutocompleteInputSpans extends ThemeableMixin('ee-autocomplete-input-spa
         }
 
         #list > span {
+          position: relative;
           display: inline-block;
           padding: 2px 5px;
           border: 1px solid #ddd;
           border-radius: 1em;
           margin: 2px;
           font-size: 0.9em;
+          padding-right: 20px;
+        }
+
+        #list > span::after {
+          content: 'x';
+          position: absolute;
+          right: 6px;
+          color: #999;
+          transform: scaleX(1.2);
+        }
+
+        #list > span:hover::after {
+          color: #333;
+        }
+
+        #list > span:hover {
+          box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+          border-color: #ccc;
+          background-color: #eee;
         }
 
         textarea {
