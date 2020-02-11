5b252d072004e631df316fbe91d2b98b64429e1e
Tony Mobily <tonymobily@gmail.com>
2020-02-11 10:02:00 +0800

    Added lit stuff to ThemableMixin so that themes don't need to use lit

--- mixins/ThemeableMixin.js
+++ mixins/ThemeableMixin.js
@@ -1,5 +1,18 @@
+import { LitElement, css } from 'lit-element'
+
 export const ThemeableMixin = (path) => (base) => {
   const common = (window.TP_THEME && window.TP_THEME.common) || (p => p)
   const theme = (window.TP_THEME && window.TP_THEME[path]) || (p => p)
-  return theme(common(base))
+  return theme(common(LitBits(base)))
+}
+
+export const LitBits = (base) => {
+  return class Base extends base {
+    static get lit () {
+      return {
+        LitElement,
+        css
+      }
+    }
+  }
 }
