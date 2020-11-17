4c714668282184d3edf98a0fa7727f2c17b3c82b
Raphael Mattos <raphael.mattos@contextaware.com.br>
2020-11-16 22:30:11 -0300

    test

--- ee-table.js
+++ ee-table.js
@@ -8,9 +8,11 @@ import { DragAndDropMixin } from './mixins/DragAndDropMixin'
 
 // https://github.com/Victor-Bernabe/lit-media-query/blob/master/lit-media-query.js
 
+// eslint-disable-next-line new-cap
 export class EeTable extends DragAndDropMixin(ThemeableMixin('ee-table')(StyleableMixin(LitElement))) {
   static get styles () {
     return [
+      super.styles || [],
       css`
         :host {
           display: block;
@@ -24,6 +26,11 @@ export class EeTable extends DragAndDropMixin(ThemeableMixin('ee-table')(Styleab
         :host([striped]) ::slotted(ee-row:nth-child(even)) {
           background-color: var(--ee-table-striped-even-color, whitesmoke)
         }
+
+        #last-row-drop-target {
+          display: none;
+          text-align: center;
+        }
       `
     ]
   }
@@ -35,7 +42,12 @@ export class EeTable extends DragAndDropMixin(ThemeableMixin('ee-table')(Styleab
       },
       medium: {
         type: String
-      }
+      },
+      dragDrop: {
+        type: Boolean,
+        attribute: 'drag-drop'
+      },
+      manipulateDOM: { type: Boolean, attribute: 'manipulate-dom' }
     }
   }
 
@@ -43,6 +55,15 @@ export class EeTable extends DragAndDropMixin(ThemeableMixin('ee-table')(Styleab
     super()
     this.small = 600
     this.medium = 1024
+    this.dragDrop = false
+    this.manipulateDOM = false
+    this._boundDragstart = this._dragstart.bind(this)
+    this._boundDragover = this._dragover.bind(this)
+    this._boundDragend = this._dragend.bind(this)
+    this._boundDragenter = this._dragenter.bind(this)
+    this._boundDragleave = this._dragleave.bind(this)
+    this._boundDragexit = this._dragexit.bind(this)
+    this._boundDragdrop = this._dragdrop.bind(this)
   }
 
   _changeRowsSize (size) {
@@ -60,8 +81,45 @@ export class EeTable extends DragAndDropMixin(ThemeableMixin('ee-table')(Styleab
     }
   }
 
+   // Sets up all list children (rows) with listeners for Drag and Drop
+  _updateDragDrop () {
+    const rows = this.shadowRoot.querySelector('slot').assignedElements()
+    for (const row of rows) {
+      if (this.dragDrop) {
+        row.setAttribute('draggable', 'true')
+        this._activateRowDnD(row)
+      } else {
+        row.removeAttribute('draggable')
+        this._deactivateRowDnD(row)
+      }
+    }
+  }
+
+  _activateRowDnD (row) {
+    row.addEventListener('dragstart', this._dragstart, false)
+    row.addEventListener('dragover', this._dragover, false)
+    row.addEventListener('dragend', this._dragend, false)
+    row.addEventListener('dragenter', this._dragenter, false)
+    row.addEventListener('dragleave', this._dragleave, false)
+    row.addEventListener('dragexit', this._dragexit, false)
+    row.addEventListener('drop', this._dragdrop, false)
+  }
+
+  _deactivateRowDnD (row) {
+    row.removeEventListener('dragstart', this._dragstart)
+    row.removeEventListener('dragover', this._dragover)
+    row.removeEventListener('dragend', this._dragend)
+    row.removeEventListener('dragenter', this._dragenter)
+    row.removeEventListener('dragleave', this._dragleave)
+    row.removeEventListener('dragexit', this._dragexit)
+    row.removeEventListener('drop', this._dragdrop)
+  }
+
   firstUpdated () {
     this._handleResize()
+    this._updateDragDrop()
+    this.lastDropTarget = this.shadowRoot.querySelector('#last-row-drop-target')
+    this._activateRowDnD(this.lastDropTarget)
   }
 
   connectedCallback () {
@@ -74,11 +132,98 @@ export class EeTable extends DragAndDropMixin(ThemeableMixin('ee-table')(Styleab
     })
   }
 
+  _dragstart (e) {
+    if (e.target.header) e.preventDefault()
+    e.dataTransfer.effectAllowed = 'move'
+    e.dataTransfer.dropEffect = 'move'
+    // This is the proper way to 'move' data around while dragging. However, webKit based browsers
+    // only make that data accessible on the drop event, so checking anything in dragover is harder.
+    // To make it simpler and fully intereoperable, the list parent (ee-table)
+    // stores the current moving item in a property
+    const table = this.parentElement
+    table.moving = this
+    requestAnimationFrame(() => {
+      this.style.visibility = 'hidden'
+      // Show last row drop target
+      if (table.manipulateDOM) table.lastDropTarget.style.display = 'block'
+    })
+    // All handler hooks are called from the list parent, which must implement them.
+    table.handleDragstart(e, table.moving)
+  }
+
+  handleDragstart (e) {}
+
+  _dragover (e) {
+    // preventDefault is necessary to ALLOW custom dragover and dropping handling
+    if (!this.header) e.preventDefault()
+    const table = this.parentElement
+    table.handleDragover(e, table.moving, this)
+  }
+
+  handleDragover (e) {}
+
+  _dragend (e) {
+    // Clear the temporary moving item reference
+    this.moving = null
+    const table = this.parentElement
+    requestAnimationFrame(() => {
+      this.style.visibility = ''
+      // Hide last row drop target
+      if (table.manipulateDOM) table.lastDropTarget.style.display = ''
+    })
+    if (this.header) e.preventDefault()
+    table.handleDragend(e, table.moving, this)
+  }
+
+  handleDragend (e) {}
+
+  _dragenter (e) {
+    // preventDefault is necessary to ALLOW custom dragenter handling
+    // console.log(this.moving)
+    if (!this.header && this !== this.moving) e.preventDefault()
+    else return
+    const table = this.parentElement
+    table.handleDragenter(e, table.moving, this)
+  }
+
+  handleDragenter (e) {}
+
+  _dragleave (e) {
+    if (this.header) e.preventDefault()
+    const table = this.parentElement
+    table.handleDragleave(e, table.moving, this)
+  }
+
+  handleDragleave (e) {}
+
+  _dragexit (e) {
+    if (this.header) e.preventDefault()
+    const table = this.parentElement
+    table.handleDragexit(e, table.moving, this)
+  }
+
+  handleDragexit (e) {}
+
+  _dragdrop (e) {
+    if (this.header) return
+    e.preventDefault()
+    const table = this.parentElement
+    table.handleDragdrop(e, table.moving, this)
+  }
+
+  handleDragdrop (e) {}
+
   render () {
     if (this.themeRender) return this.themeRender()
     return html`
-      <slot @slotchange="${() => { this._handleResize() }}"></slot>
+      <slot @slotchange="${this._slotChanged}"></slot>
+      <ee-row id="last-row-drop-target">target for last item</ee-row>
     `
+  }
+
+  _slotChanged () {
+    this._handleResize()
+    this._updateDragDrop()
   }
 }
 customElements.define('ee-table', EeTable)
