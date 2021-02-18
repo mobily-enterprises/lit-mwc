5c1c602c0a55d0f9b0af37f9351653f0b775eba8
freudFlintstone <raphael.mattos@contextaware.com.br>
2021-02-18 19:31:26 -0300

    initial layout and styling work

--- docs-src/makeDocs.sh
+++ docs-src/makeDocs.sh
@@ -6,20 +6,7 @@ echo "tpelements.com" > ../docs/CNAME
   -c tpe.css\
   -t tpe.ejs\
   -o ../docs\
- index.md\
- quickstart-designers.md\
- tutorials-designers.md\
- tutorials-designers/*\
- quickstart-developers.md\
- tutorials-developers.md\
- tutorials-developers/*\
- elements.md\
- elements/ee*.js\
- elements/en*.js\
- elements/nn*.js\
- mixins.md\
- mixins/*.js\
- appendices.md\
- appendices/*.md\
- material-theme.md
+  get-started.md\
+  documentation.md\
+  codelabs.md
 cp -r ./images ../docs/
